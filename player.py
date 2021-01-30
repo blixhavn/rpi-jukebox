50095bdc360a70181ec676c50c4d6767fdf98b4c
Øystein Blixhavn <oeb@itverket.no>
2021-01-30 12:05:12 +0100

    fix bug

--- player.py
+++ player.py
@@ -12,7 +12,7 @@ def main():
         status = -1
         while status != RC522.OK:
             status, msg = reader.read_blocking()
-            sleep(0.01)
+            time.sleep(0.01)
 
         
         requests.get(f'http://192.168.0.181:5005/Stue/spotify/now/{msg}')
