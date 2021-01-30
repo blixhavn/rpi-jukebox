aab55ad23d2b8dc7ce545078b5a131ef7a7086bb
Øystein Blixhavn <oeb@itverket.no>
2021-01-30 12:04:35 +0100

    add flick

--- player.py
+++ player.py
@@ -1,13 +1,14 @@
 import RC522
 import requests
 import time
+import flicklib
 
 from RC522_NTAG21X import RC522_NTAG21X
 
 def main():
     reader = RC522_NTAG21X()
 
-    while true:
+    while True:
         status = -1
         while status != RC522.OK:
             status, msg = reader.read_blocking()
@@ -16,3 +17,9 @@ def main():
         
         requests.get(f'http://192.168.0.181:5005/Stue/spotify/now/{msg}')
 
+@flicklib.airwheel()
+def volume_ctrl(delta)
+    print(delta)
+
+if __name__ == '__main__':
+    main()
